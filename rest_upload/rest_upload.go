b6417047ad694a24db564bf3820ceae192b785ef
sampila <alipxsulistio@gmail.com>
2020-04-05 22:42:17 +0700

    fixing upload rest client

--- rest_upload/rest_upload.go
+++ rest_upload/rest_upload.go
@@ -2,14 +2,13 @@ package rest_upload
 
 import (
 	"strings"
-	"github.com/mercadolibre/golang-restclient/rest"
-	"time"
 	"encoding/json"
   "mime/multipart"
 	"github.com/sampila/go-utils/rest_errors"
 	"errors"
   "bytes"
   "io/ioutil"
+	"net/http"
 )
 
 /*const (
@@ -20,53 +19,58 @@ import (
 	paramAccessToken = "access_token"
 )*/
 
-var (
-	uploadRestClient = rest.RequestBuilder{
-		BaseURL: "http://localhost:9010",
-    ContentType: rest.MULTIPART,
-		Timeout: 3000 * time.Millisecond,
-	}
-)
-
 type FileRequest struct {
   StoreCode string  `json:"store_code"`
+  Type 			string  `json:"type"`
   File *multipart.FileHeader `json:"file"`
 }
 
-func UploadFile(p *FileRequest) (interface{}, rest_errors.RestErr) {
-  bodyBuf := &bytes.Buffer{}
-  bodyWriter := multipart.NewWriter(bodyBuf)
+func UploadFile(p *FileRequest) (*map[string]interface{}, rest_errors.RestErr) {
   file, err := p.File.Open()
   defer file.Close()
   if err != nil {
-		return "", rest_errors.NewInternalServerError("invalid error interface when trying to upload file", err)
+		return nil, rest_errors.NewInternalServerError("invalid error interface when trying to upload file", err)
 	}
 	fileData, err := ioutil.ReadAll(file)
 	if err != nil {
-		return "", rest_errors.NewInternalServerError("invalid error interface when trying to upload file", err)
+		return nil, rest_errors.NewInternalServerError("invalid error interface when trying to upload file", err)
 	}
+
+	bodyBuf := new(bytes.Buffer)
+	bodyWriter := multipart.NewWriter(bodyBuf)
+	fw, _ := bodyWriter.CreateFormFile("file", p.File.Filename)
+	fw.Write(fileData)
   bodyWriter.WriteField("store_code", p.StoreCode)
-  bodyWriter.WriteField("file", string(fileData))
+  bodyWriter.WriteField("type", p.Type)
   bodyWriter.Close()
-	response := uploadRestClient.Post("/v1/file", bodyBuf)
-	if response == nil || response.Response == nil {
-		return nil, rest_errors.NewInternalServerError("invalid restclient response when trying to upload file",
+	request, err := http.NewRequest("POST", "http://localhost:9010/v1/file", bodyBuf)
+	if err != nil {
+		return nil, rest_errors.NewInternalServerError("error on assign request parameter", err)
+	}
+	request.Header.Add("Content-Type", bodyWriter.FormDataContentType())
+
+	client := &http.Client{}
+	response, respErr := client.Do(request)
+	if respErr != nil {
+		return nil, rest_errors.NewInternalServerError("invalid response",
 			errors.New("network timeout"))
 	}
+	defer response.Body.Close()
+	bodyContent, _ := ioutil.ReadAll(response.Body)
+
 	if response.StatusCode > 299 {
-		if strings.TrimSpace(response.String()) == "expired access token" {
-			restErr := rest_errors.NewUnauthorizedError(response.String())
+		if strings.TrimSpace(string(bodyContent)) == "expired access token" {
+			restErr := rest_errors.NewUnauthorizedError(string(bodyContent))
 			return nil, restErr
 		}
-		restErr, err := rest_errors.NewRestErrorFromBytes(response.Bytes())
+		restErr, err := rest_errors.NewRestErrorFromBytes(bodyContent)
 		if err != nil {
 			return nil, rest_errors.NewInternalServerError("invalid error interface when trying to upload file", err)
 		}
 		return nil, restErr
 	}
-
-	var respBody interface{}
-	if err := json.Unmarshal(response.Bytes(), &respBody); err != nil {
+	var respBody map[string]interface{}
+	if err := json.Unmarshal(bodyContent, &respBody); err != nil {
 		return nil, rest_errors.NewInternalServerError("error when trying to unmarshal response",
 			errors.New("error processing json"))
 	}
